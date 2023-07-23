1018986f5c581d8fc7e53002d2ed9764817a7706
Rinat N <rinat-n@auto.kz>
2023-07-23 12:08:02 +0600

    dev: fix text on main page

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Welcome</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
